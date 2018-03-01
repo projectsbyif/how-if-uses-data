e7b3bf28963a4e025e380c2a6c582226cf1ccebf
Paul M Furley <paul@paulfurley.com>
2018-03-01 15:40:04 +0000

    Add code & Makefile to filter Markdown through Jinja
    ... so that we can use `{% include "matomo.html" %}`, for example

---
+++ add_microformats.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""
+Render a Markdown template through Jinja
+"""
+
+import io
+import sys
+
+from os.path import abspath, dirname, join as pjoin
+
+from jinja2 import Environment, FileSystemLoader
+
+
+SCHEMA_DIR = abspath(pjoin(dirname(__file__), 'schema'))
+THIS_DIR = abspath(pjoin(dirname(__file__), ''))
+
+
+def main(input_markdown, output_markdown):
+
+    env = Environment(loader=FileSystemLoader([THIS_DIR, SCHEMA_DIR]))
+    template = env.get_template(input_markdown)
+    rendered_markdown = template.render()
+
+    with io.open(output_markdown, 'w') as f:
+        f.write(rendered_markdown)
+
+    print("\nOutput written to {}".format(output_markdown))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Usage: {} <file.md> <output_file.md>".format(sys.argv[0]))
+        sys.exit(1)
+    else:
+        main(sys.argv[1], sys.argv[2])
